29dca66a6634a25e6a91a5e4a873dd1fcc9159f3
channy-univ <channy.univ@gmail.com>
2023-08-20 22:03:21 +0700

    feat: add hero section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
       <Footer />
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
     extend: {
       colors: {
         primary: "#3E84DA",
+        danger: "#E9B43A",
+      },
+      backgroundImage: {
+        "gradient-229": "linear-gradient(229deg, var(--tw-gradient-stops))",
       },
     },
   },
